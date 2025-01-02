c2714d8d267e80d53aea871c017645e5ef00907a
StasAlex <salexandronets@corp.web4pro.net>
2025-01-02 20:37:19 +0200

    Fix auth redirect issue

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -18,7 +18,22 @@ const authOptions = {
         async redirect({ url, baseUrl }: { url: string; baseUrl: string }) {
             return url.startsWith(baseUrl) ? "/dashboard" : baseUrl;
         },
+        async session({ session, token }:{ session: any; token: string | any }) {
+            console.log("session", session);
+            if (token) {
+                session.user.id = token.id;
+            }
+            return session;
+        },
+        async jwt({ token, user }:{ token: string | any; user: any }) {
+            console.log("user", user);
+            if (user) {
+                token.id = user.id;
+            }
+            return token;
+        },
     },
+
 };
 
 const handler = NextAuth(authOptions)
